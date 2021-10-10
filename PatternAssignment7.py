cbefa01a144fe059a17d7604ee09f92f93cb02b7
deepanshubaghel <92099428+deepanshubaghel@users.noreply.github.com>
2021-10-10 09:54:37 +0530

    Delete PatternAssignment7.py

--- PatternAssignment7.py
+++
@@ -1,8 +0,0 @@
-rows = 5
-for i in range(1, rows + 1):
-    for j in range(1, rows + 1):
-        if j <= i:
-            print(i, end=' ')
-        else:
-            print(j, end=' ')
-    print()
